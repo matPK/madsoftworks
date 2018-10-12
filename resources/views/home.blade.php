49f3f0605caf3599c7be3a840b738939a9c424b8
Matheus Dardenne <mat.dardenne@gmail.com>
2018-10-12 16:51:03 -0300

    design home aprimorado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="home" class="m-4">
-	<div class="row">
-		<div class="col-md-5">
+<div id="home" class="container">
+	<section id="intro" class="block row">
+		<div class="col-md-6 m-auto">
 			<div class="home-image">
-				<img src="{{asset('img/mat_foto.jpg')}}" alt="Matheus Adorni Dardenne">
+				<img src="{{asset('img/MAD.jpg')}}" alt="Logo MAD SoftWorks">
 			</div>
 		</div>
-		<div class="col-md-7">
+		<div class="col-md-6 m-auto">
 			<h2>O que é um artesão web?</h2>
 			<p>
-				Artesão é um profissional que fabrica produtos através de um processo manual ou com auxílio de ferramentas. Sua profissão usualmente requer algum tipo de habilidade ou conhecimento especializado na sua prática. O artesão web usa seu conhecimento especializado, sua habilidade e as mais modernas ferramentas para criar aplicações web inspiradores que se destacam na multidão.
+				Artesão é um profissional que fabrica produtos através de um processo manual ou com auxílio de ferramentas. Sua profissão usualmente requer algum tipo de habilidade ou conhecimento especializado na sua prática. O artesão web usa seu conhecimento especializado, sua habilidade e as mais modernas ferramentas para criar aplicações web inspiradoras que se destacam na multidão.
 			</p>
+			<span>- Matheus A. Dardenne</span>
 		</div>
-	</div>
+	</section>
+	
 </div>
 @endsection
